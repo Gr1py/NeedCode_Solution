033eb856133cb41585e28ddc236b459611fdd209
Otakar <Otaka@seznam.cz>
2024-04-30 16:19:53 +0200

    Adding soluttion of Contains_Duplicate

---
+++ Contains_Duplicate.py
@@ -0,0 +1,12 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+nums = [1,2,3,1]
+#Output: true
+
+n = len(nums)
+for i in range(n):
+    for j in range(i + 1, n):
+        if nums[i] == nums[j]:
+            #return True
+            print(True)
+#return False
+print (False)
